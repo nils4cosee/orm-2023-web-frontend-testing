3a72f210f3aa96b799d2f5e2e00cf71d852bef1b
Nils Knappmeier <nils.knappmeier@cosee.biz>
2023-09-22 13:18:01 +0200

    fix tests

--- my-vue-app/src/pages/CounterSum.test.ts
+++ my-vue-app/src/pages/CounterSum.test.ts
@@ -4,6 +4,7 @@ import CounterSum from "./CounterSum.vue";
 import { screen } from "@testing-library/vue";
 import userEvent from "@testing-library/user-event";
 import {copyToClipboard} from "../browser/copyToClipboard.ts";
+import {debug} from "vitest-preview";
 
 type UserEvent = ReturnType<typeof userEvent.setup>;
 
@@ -24,7 +25,7 @@ describe("CounterSum", () => {
   it("updates the sum on click", async () => {
     const { screen, user } = renderComponent(CounterSum, {});
     await incrementCounter(user, "One");
-    await incrementCounter(user, "Two");
+    await incrementCounter(user, "Counter");
     expect(screen.getByText("Sum:", { exact: false })).toHaveTextContent(
       "Sum: 2",
     );
@@ -32,8 +33,9 @@ describe("CounterSum", () => {
 
   it("Copies the sum to the clipboard", async () => {
     const { screen, user } = renderComponent(CounterSum, {});
+    debug()
     await incrementCounter(user, "One");
-    await incrementCounter(user, "Two");
+    await incrementCounter(user, "Counter");
     await user.click(screen.getByText("Copy to clipboard"))
     expect(copyToClipboard).toHaveBeenCalledWith("2")
   })
